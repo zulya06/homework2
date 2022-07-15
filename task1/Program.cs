be0eae1cf4f9d382a93569d62bbbf06acbf6f99b
 zulya06 <kurbanova.z1990@mail.ru>
2022-07-15 23:40:02 +0300

    creatr task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void Seconddigit (int num1)
+{
+int twodid = num1/10;
+int result = twodid % 10;
+Console.WriteLine("Seconddigit is" + result);
+
+}
+
+Console.WriteLine(" Input there digit num1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Seconddigit(num1);
