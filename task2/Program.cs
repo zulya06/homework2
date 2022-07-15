3b0623c1d414a4072dc2f24657a72f8e3c39b4e5
 zulya06 <kurbanova.z1990@mail.ru>
2022-07-16 00:11:14 +0300

    Creatr task2

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+void Thirddigit (string num)
+{
+    if (num.Length >= 3)
+    {
+       char thirdChar = num [2];
+       Console.WriteLine("Third digit is " + thirdChar);
+    }
+    
+    else
+    {
+        if (num.Length == 2 || num.Length == 1)
+        {
+            Console.WriteLine("This num has no third digit");
+        }
+    }
+    
+}
+
+string num1;
+Console.Write("Input your num: ");
+num1 = Console.ReadLine();
+Thirddigit (num1);
