ecb4991a459e338a512c3c91658c442006e53751
 zulya06 <kurbanova.z1990@mail.ru>
2022-07-16 00:59:56 +0300

    creatr task3

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+void fineday (int num)
+
+{
+if (num >=5)
+{
+    Console.WriteLine( "this is not weeknd");
+}
+if (num ==6  || num ==7 )
+{
+  Console.WriteLine (" this is weeknd");
+}
+
+}
+
+Console.WriteLine("input num");
+  int num = Convert.ToInt32 (Console.ReadLine()); 
+fineday (num);
+
